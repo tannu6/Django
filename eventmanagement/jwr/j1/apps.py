abdbb0cf5951620800febdf6cfd39987afcf64b3
Tannu Sharma <tsharma4563@gmail.com>
2026-07-23 01:01:06 +0530

    Delete eventmanagement directory

--- eventmanagement/jwr/j1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class J1Config(AppConfig):
-    name = 'j1'
--- eventmanagement/pb_537/p1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class P1Config(AppConfig):
-    name = 'p1'
